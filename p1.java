78b5ae5a509e4a1621646f01bad0c8bbbba88c4f
hemant11-hr <lhrhemant@gmail.com>
2025-01-01 17:27:53 +0530

    Add files via upload

---
+++ p1.java
@@ -0,0 +1,23 @@
+public class p1 {
+    public static void main(String[]args){
+        int a=10;
+        int b=20;
+        int e=a+b;
+        System.out.println("Sum is -"+e);
+        System.out.println("Minus is -"+(a-b));
+
+        boolean c=true;
+        boolean d=false;
+        System.out.println("And is-"+(c&&d));
+        System.out.println("or is-"+(c||d));
+        System.out.println("Not is-"+(!d));
+
+
+    }
+       
+
+
+    
+
+    
+}
